079766c3b22258f728d643aad34cb07daa4a2226
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2023-12-20 00:08:13 +0530

    using Image component in next js

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 import {lusitana} from './ui/font';
-
+import { Head } from 'next/document';
 export default function Page() {
   return (
+    <>
+ 
     <main className={`flex min-h-screen flex-col p-6 ${lusitana.className}`}   >
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
@@ -18,6 +19,13 @@ export default function Page() {
         height={760}
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
+      />
+          <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
       />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
@@ -38,5 +46,6 @@ export default function Page() {
         </div>
       </div>
     </main>
+    </>
   );
 }
